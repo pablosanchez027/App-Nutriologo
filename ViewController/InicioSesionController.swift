b02651c04248357a14f6a757062181559802fad8
Alumno <ulsa@iMacD107.local>
2019-05-07 19:38:03 -0700

    Validacion completa

--- ViewController/InicioSesionController.swift
+++ ViewController/InicioSesionController.swift
@@ -11,5 +11,19 @@ import UIKit
 
 class InicioSesionController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var txtCorreo: UITextField!
+    @IBOutlet weak var txtContra: UITextField!
+    @IBOutlet weak var lblAviso: UILabel!
+    
+    
+    @IBAction func doTapEntrar(_ sender: Any) {
+        if txtCorreo.text != nil && txtCorreo.text != "" && txtContra.text != nil && txtContra.text != "" {
+            //Validación
+            performSegue(withIdentifier: "goToEntrar", sender: self)
+        } else {
+            //No pasó
+            lblAviso.isHidden = false
+        }
+    }
     
 }
